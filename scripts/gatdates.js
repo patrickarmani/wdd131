df42d47a04a6f739468389ab610cc2a71701c1cf
Patrick Armani <parmani@byupathway.edu>
2026-07-08 15:10:47 -0300

    "Final adjustments for W01"

--- scripts/gatdates.js
+++ scripts/gatdates.js
@@ -4,6 +4,6 @@ const today = new Date();
 
 currentyear.innerHTML = `<span id="currentyear"> @ ${today.getFullYear()} - Patrick Marcelo A. S. Armani - Brazil</span>`;
 
-const modified = new Date();
+
 
 const lasModified = document.getElementById("lastModified").innerHTML = document.lastModified;
