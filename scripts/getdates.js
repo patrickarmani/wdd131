9d55b2d17b07e0ae2d733a1f6784583e1c29950c
Patrick Armani <parmani@byupathway.edu>
2026-07-09 17:46:11 -0300

    "Changing getgates.js"

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,8 +2,8 @@ const currentyear = document.querySelector("#currentyear");
 
 const today = new Date();
 
-currentyear.innerHTML = `<span id="currentyear"> \u00A9 ${today.getFullYear()} - Patrick Marcelo A. S. Armani - Brazil</span>`;
-
+// currentyear.innerHTML = `<span id="currentyear"> \u00A9 ${today.getFullYear()} - Patrick Marcelo A. S. Armani - Brazil</span>`;
+currentyear.textContent = today.getFullYear();
 
 
 const lasModified = document.getElementById("lastModified").innerHTML = document.lastModified;
