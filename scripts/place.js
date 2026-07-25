22f9a15e9a03d67831e4d48c99774c2d2a1a9b74
Patrick Armani <parmani@byupathway.edu>
2026-07-25 15:22:02 -0300

    Fix country page rubric requirements

--- scripts/place.js
+++ scripts/place.js
@@ -1,18 +1,15 @@
 const temperature = 10;
 const windSpeed = 5;
 
-const calculateWindChill = (temperature, windSpeed) =>
-    13.12 +
-    (0.6215 * temperature) -
-    (11.37 * Math.pow(windSpeed, 0.16)) +
-    (0.3965 * temperature * Math.pow(windSpeed, 0.16));
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + (0.6215 * temperature) - (11.37 * Math.pow(windSpeed, 0.16)) + (0.3965 * temperature * Math.pow(windSpeed, 0.16));
+}
 
 const windChillElement = document.querySelector("#wind-chill");
 
 if (temperature <= 10 && windSpeed > 4.8) {
-    const windChill = calculateWindChill(temperature, windSpeed);
-
-    windChillElement.textContent = `${windChill.toFixed(1)} °C`;
+    windChillElement.textContent =
+        `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
 } else {
     windChillElement.textContent = "N/A";
 }
