f76cd228ee514f217ef041cfb03e30126a8a6d0e
Patrick Marcman <113485106+patrickarmani@users.noreply.github.com>
2026-07-28 17:22:01 +0000

    "Add getdates to lazyload"

---
+++ week04/scripts/getdates.js
@@ -0,0 +1,9 @@
+const currentyear = document.querySelector("#currentyear");
+
+const today = new Date();
+
+// currentyear.innerHTML = `<span id="currentyear"> \u00A9 ${today.getFullYear()} - Patrick Marcelo A. S. Armani - Brazil</span>`;
+currentyear.textContent = today.getFullYear();
+
+
+const lasModified = document.getElementById("lastModified").innerHTML = document.lastModified;
