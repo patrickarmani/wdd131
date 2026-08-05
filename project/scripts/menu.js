bc4a720252645060188e51c16bd2bdaf3719807f
Patrick Marcman <113485106+patrickarmani@users.noreply.github.com>
2026-08-05 18:39:49 +0000

    " adding menu"

---
+++ project/scripts/menu.js
@@ -0,0 +1,11 @@
+const menuButton = document.querySelector('#menu-button');
+const navigation = document.querySelector('#navigation');
+
+menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    menuButton.classList.toggle("open");
+
+    const isOpen = navigation.classList.contains("open");
+
+    menuButton.setAttribute("aria-expanded", isOpen);
+});
